6b0cc60df18135371e4f663f0454ac528a6d991f
Riddhidipta Pal <riddhidipta2003@gmail.com>
2023-12-21 14:05:58 +0530

    yfyfi

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
-function App() {
-  return (
-    <div className="App">
+import { ColorModeContext, useMode } from "./theme";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { Topbar } from "./scenes/global/Topbar";
 
-    </div>
+function App() {
+  const [theme, colorMode] = useMode();
+
+  return (
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <div className="app">
+          <main className="content">
+            <Topbar />
+          </main>
+        </div>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
   );
 }
 
